9f16c82b1a85acbe3759f59d715f732cab3f2468
Roger Araya <roger.araya@pfcti.com>
2023-05-03 16:20:42 -0600

    Clase 11, subida

--- src/main/java/com/pfcti/spring/dev/app/config/SecurityConfig.java
+++ src/main/java/com/pfcti/spring/dev/app/config/SecurityConfig.java
@@ -14,28 +14,31 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(); manager.createUser(User.withUsername("user")
-                .password("{noop}userPass") .roles("USER")
-                .build());
-        manager.createUser(User.withUsername("admin") .password("{noop}adminPass")
-                .roles("USER", "ADMIN")
-                .build());
-        return manager; }
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(); manager.createUser(User.withUsername("user")
+//                .password("{noop}userPass") .roles("USER")
+//                .build());
+//        manager.createUser(User.withUsername("admin") .password("{noop}adminPass")
+//                .roles("USER", "ADMIN")
+//                .build());
+//        return manager; }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf() .disable()
-                .authorizeRequests()
-                .requestMatchers(HttpMethod.DELETE)
-                .hasRole("ADMIN")
-                .requestMatchers("/v1/api/cliente/**","/v1/api/employee/**")
-                .hasAnyRole("USER", "ADMIN") .requestMatchers("v1/api/login/**")
-                .anonymous() .anyRequest() .authenticated() .and() .httpBasic();
-        return http.build(); }
+        http.csrf().disable().authorizeRequests().requestMatchers(HttpMethod.DELETE).
+                hasRole("ADMIN").requestMatchers("/v1/api/cliente/**", "/v1/api/employee/**").
+                hasAnyRole("USER", "ADMIN").
+                requestMatchers("v1/api/login/**").
+                anonymous().anyRequest().
+                authenticated().
+                and().
+                oauth2ResourceServer().jwt()
+                .jwtAuthenticationConverter(new CustomJwtAuthenticationConverter());
+        return http.build();
+    }
 }
